7b74de8078b4b35bbe0bc6208de822f9fa656d1c
Jorge Astorga <jorge.astorga@jorgeastor-ltm1.hsd1.ca.comcast.net>
2014-08-07 23:59:31 -0700

    added the setup.sh file to setup the environment

---
+++ webapp/setup.sh
@@ -0,0 +1,11 @@
+#Create your own (project)environment 
+virtualenv venv
+
+#Activate the environment
+. venv/bin/activate
+
+#Install the flask module
+pip install flask
+
+#Run the application
+#python hello.py
